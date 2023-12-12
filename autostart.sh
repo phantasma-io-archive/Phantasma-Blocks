8f4649aa186d7f558f4693dd4cc4f5799d7e82da
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-12-12 12:40:37 +0000

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,7 @@
 
 # Setup the user path so you can have access to the bun command
 #PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-cd /home/jnovo/Phantasma/Phantasma-Blocks/
+cd /home/path/to/Phantasma/Phantasma-Blocks/
 while :
 do
     # If the command exits (either successfully or due to an error), the following line will be executed
